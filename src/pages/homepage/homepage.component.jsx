a5ab43f4ce9b63db1e9d8b624641a3b15b404d8d
Sandra Naim <sandra.naim@toothpickapp.com>
2021-12-26 20:26:42 +0200

    let the component be expost as default since it is required when using lazy react

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Directory from "../../components/directory/directory.component";
 import { HomePageContainer } from "./homepage.styles";
 
-export const HomePage = () => (
+const HomePage = () => (
     <HomePageContainer>
         <Directory />
     </HomePageContainer>
 )
+
+export default HomePage;
