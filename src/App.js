6c556264670c38af65fa81a8d860c880b9c10b9e
Sandra Naim <sandra.naim@toothpickapp.com>
2021-12-26 16:50:53 +0200

    create a global style file instead of App css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import { GlobalStyle } from './global.styles';
+
 import { HomePage } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -47,6 +48,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
+        <GlobalStyle />
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
